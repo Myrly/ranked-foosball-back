7962e25495e6828c4c81d97db055d5f9b784880c
Myrly <maconnerie-legendaire.0o@icloud.com>
2024-08-29 09:30:28 +0200

    Include connection to MongoDB via dotenv file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
 import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
   imports: [
     PlayerModule,
-    MongooseModule.forRoot('<uri>', {
-      dbName: 'foosball',
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_CREDENTIALS_URI'),
+        dbName: 'foosball',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
